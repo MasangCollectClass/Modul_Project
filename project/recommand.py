5a3cfe06739645d04d00059b1ff0028709a777c4
Gosuke716 <gosu716@naver.com>
2025-07-04 10:15:09 +0900

    chat.py 드롭박스 추가, recommand 페이지 더미데이터 추가

--- project/recommand.py
+++ project/recommand.py
@@ -7,9 +7,15 @@ tabs = st.tabs(["여행지 추천", "노래 추천"])
 
 # 여행지 추천 탭
 with tabs[0]:
-    st.container(border=True).video(
-        "https://www.youtube.com/watch?v=exJF_Y3QFbg", autoplay=False
-    )
+    cols = st.columns(3)
+    with cols[0].container(border=False):
+        st.header("여행지")
+        st.markdown("*제주도*")
+        st.subheader("추천 이유")
+        st.markdown("*넓은 바다와 멋진 풍경!*")
+    with cols[1].container(border=True):
+        st.image("project/images/jeju.jpg", use_container_width=False)
+        st.link_button("링크", url="https://visitjeju.net/kr", icon=":material/open_in_new:")
 
 # 노래 추천 탭
 with tabs[1]:
